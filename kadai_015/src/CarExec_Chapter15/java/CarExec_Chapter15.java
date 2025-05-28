a2eeaeaa339c6ea1362aadbbeb35455a8370ebb0
obunrose <obun1360eno1927@gmail.com>
2025-05-29 07:27:45 +0900

    Add files via upload

---
+++ kadai_015/src/CarExec_Chapter15/java/CarExec_Chapter15.java
@@ -0,0 +1,13 @@
+package CarExec_Chapter15.java;
+
+import Car_Chapter15.java.Car_Chapter15;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 start = new Car_Chapter15();
+		start.gearChange(3);
+		start.run();
+	}
+
+}
