0b2b5c4f1be6440a45cc42f341ab6eed3e5e8808
obunrose <obun1360eno1927@gmail.com>
2025-05-30 20:51:47 +0900

    課題提出１

---
+++ src/kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+
+	public KatoHanako_Chapter18() {
+		super();
+	}
+
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	@Override
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です\n");
+
+	}
+
+}
