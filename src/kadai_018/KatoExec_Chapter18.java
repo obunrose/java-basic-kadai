852e3ddcf2921039e03fca90b769f84f7aede082
obunrose <obun1360eno1927@gmail.com>
2025-05-30 21:37:01 +0900

    Add files via upload

---
+++ src/kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 ici = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter18 hana = new KatoHanako_Chapter18();
+
+		taro.execIntroduce();
+		ici.execIntroduce();
+		hana.execIntroduce();
+
+	}
+
+}
---
+++ src/kadai_018/Kato_Chapter18.java
@@ -0,0 +1,24 @@
+package kadai_018;
+
+abstract class Kato_Chapter18 {
+
+	public String familyName;
+	public String givenName;
+	public String address;
+
+	//共通の紹介を出力する
+	public void commonIntroduce() {
+		address = "東京都中野区〇×";
+		System.out.println("住所は" + address + "です");
+	}
+
+	//個別の紹介を出力する
+	public abstract void eachIntroduce();
+
+	//紹介を実行する
+	public void execIntroduce() {
+		System.out.println("名前は加藤" + givenName + "です");
+		commonIntroduce();
+		eachIntroduce();
+	}
+}
